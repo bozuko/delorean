3c39171672e816e0bcf4601df0b65fdbf1e9db04
Andrew Stone <andrew.j.stone.1@gmail.com>
2012-03-28 04:50:21 -0400

    use svg

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,32 @@ window.onload = function() {
   var plays = d3.select('#plays');
   var data = [4, 8, 15, 16, 23, 42];
 
+  // Scaling Functions 
+  var x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, 420]);
+
+  var y = d3.scale.ordinal()
+    .domain(data)
+    .rangeBands([0, 120]);
+
+  // Generate the bars for the 'plays' chart
   plays
-  .selectAll("div")
+  .selectAll('rect')
   .data(data)
-  .enter().append('div')
-  .style("width", function(d) { return d * 10 + "px"; })
-  .text(function(d) { return d; });
+  .enter().append('rect')
+  .attr('y', function(d, i) { return i*20;})
+  .attr('width', x)
+  .attr('height', y.rangeBand());
+
+  // Generate the text for the 'plays' chart
+  plays.selectAll("text")
+    .data(data)
+    .enter().append("text")
+    .attr("x", x)
+    .attr("y", function(d) { return y(d) + y.rangeBand() / 2; })
+    .attr("dx", -3) // padding-right
+    .attr("dy", ".35em") // vertical-align: middle
+    .attr("text-anchor", "end") // text-align: right
+    .text(String);
 };
